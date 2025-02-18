b1165931baa2028975eb96c77124ec0392702c31
nextuser <nextuser@163.com>
2025-02-18 18:44:56 +0800

    add seed string when call findProgramAddressSync

--- client/pda-addr.ts
+++ client/pda-addr.ts
@@ -1,8 +1,9 @@
 import { PublicKey } from "@solana/web3.js";
+import { Buffer } from "buffer";
 
 const programId = new PublicKey("11111111111111111111111111111111");
 
-const [PDA, bump] = PublicKey.findProgramAddressSync([], programId);
+const [PDA, bump] = PublicKey.findProgramAddressSync([Buffer.from("pda_seed")], programId);
 
 console.log(`PDA: ${PDA}`);
 console.log(`Bump: ${bump}`);
